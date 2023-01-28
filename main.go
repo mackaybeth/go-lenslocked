3bf386336a7dd38950417b35e4648bf23ef15450
Beth MacKay <bmackay@nerdwallet.com>
2023-01-28 17:44:32 -0500

    section 2: http.handlerFunc type first way

--- main.go
+++ main.go
@@ -20,33 +20,35 @@ func pageNotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Page Not Found</h1><p>Path not supported: "+r.URL.Path)
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		//pageNotFoundHandler(w, r)
-// 		http.Error(w, http.StatusText(http.StatusNotFound)+": "+r.URL.Path, http.StatusNotFound)
-// 	}
-// }
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
+		//pageNotFoundHandler(w, r)
 		http.Error(w, http.StatusText(http.StatusNotFound)+": "+r.URL.Path, http.StatusNotFound)
 	}
 }
 
+// type Router struct{}
+
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, http.StatusText(http.StatusNotFound)+": "+r.URL.Path, http.StatusNotFound)
+// 	}
+// }
+
 func main() {
-	var router Router
+	var router http.HandlerFunc
+	// This works because pathHandler matches the underlying type for http.HandlerFunc
+	router = pathHandler
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe("localhost:3000", router)
 }
