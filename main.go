08d4658ccd9ab705dfa303b1bb0d53654e549658
Beth MacKay <bmackay@nerdwallet.com>
2023-01-29 12:37:16 -0500

    section 2: FAQ exercise

--- main.go
+++ main.go
@@ -15,6 +15,18 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:email@email.com\">email@email.com</a>.")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Frequently Asked Questions</h1>
+	<h2>Q: Is there a free version?</h2>
+	<p><b>A:</b> Yes! We offer a free trial for 30 days</p>
+	<h2>Q: What are your support hours?</h2>
+	<p><b>A:</b> 24/7 email support, slower on weekends</p>
+	<h2>Q: How do Io contact support?</h2>
+	<p><b>A:</b> email <a href="mailto:support@email.com">support@email.com</a></p>
+	`)
+}
+
 func pageNotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>Page Not Found</h1><p>Path not supported: "+r.URL.Path)
@@ -40,6 +52,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, http.StatusText(http.StatusNotFound)+": "+r.URL.Path, http.StatusNotFound)
 	}
