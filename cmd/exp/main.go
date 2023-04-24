6498a835e3536c50765fe492c8dd03b516aa678b
Beth MacKay <bmackay@nerdwallet.com>
2023-04-24 10:43:00 -0400

    sec4-contextual-encoding add script example

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -8,6 +8,7 @@ import (
 type User struct {
 	Name string
 	Bio  string
+	Age  int
 }
 
 func main() {
@@ -23,6 +24,7 @@ func main() {
 	user := User{
 		Name: "Jon Calhoun",
 		Bio:  `<script>alert("Haha, you have been h4x0r3d!");</script>`,
+		Age:  123,
 	}
 
 	// Execute is how you process a template
