1f7ea4290e9ba4d429497843418d5202cd20a0b9
Beth MacKay <bmackay@nerdwallet.com>
2023-04-24 09:56:29 -0400

    with anonymous struct

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -11,12 +11,17 @@ type User struct {
 
 func main() {
 
+	// Filepath is relative to where you're running
 	t, err := template.ParseFiles("hello.gohtml")
 	if err != nil {
 		panic(err)
 	}
 
-	user := User{
+	// Defining like this is an anonymous struct, declared inline.
+	// Second set of curly braces is instantiating the struct
+	user := struct {
+		Name string
+	}{
 		Name: "Susan Smith",
 	}
 
