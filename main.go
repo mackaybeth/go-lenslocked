3b7b6211de5fbea092a772a44bcb6439f640bed6
Beth MacKay <bmackay@nerdwallet.com>
2023-04-24 11:15:50 -0400

    sec4-contact-via-template refactor handling, added contact via template

--- main.go
+++ main.go
@@ -10,10 +10,9 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "home.gohtml") // makes the path os-agnostic
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filePath)
 	if err != nil {
 		log.Printf("parsing the template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
@@ -31,9 +30,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml") // makes the path os-agnostic
+	executeTemplate(w, tplPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:email@email.com\">email@email.com</a>.")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
