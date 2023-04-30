252c921926819a56298151c781e4d8830f20bbb2
Beth MacKay <bmackay@nerdwallet.com>
2023-04-30 12:02:04 -0400

    use is to check error type

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,36 +1,28 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"errors"
+	"fmt"
 )
 
-type User struct {
-	Name string
-	Bio  string
-	Age  int
+func main() {
+	err := B()
+	if errors.Is(err, ErrNotFound) {
+		fmt.Println("not found")
+	}
+	// TODO: Determine if the `err` variable is an `ErrNotFound`
 }
 
-func main() {
+var ErrNotFound = errors.New("not found")
 
-	// Filepath is relative to where you're running
-	t, err := template.ParseFiles("hello.gohtml")
+func A() error {
+	return ErrNotFound
+}
+
+func B() error {
+	err := A()
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("b: %w", err)
 	}
-
-	// Defining like this is an anonymous struct, declared inline.
-	// Second set of curly braces is instantiating the struct
-	user := User{
-		Name: "Jon Calhoun",
-		Bio:  `<script>alert("Haha, you have been h4x0r3d!");</script>`,
-		Age:  123,
-	}
-
-	// Execute is how you process a template
-	err = t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
-
+	return nil
 }
