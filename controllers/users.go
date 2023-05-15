70af61e88e702b5790d01c5c7642e9900d3f154c
Beth MacKay <bmackay@nerdwallet.com>
2023-05-15 11:18:30 -0400

    parse with FormValue instead

--- controllers/users.go
+++ controllers/users.go
@@ -18,14 +18,9 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 
 // Parsing the values from the form using helper methosds in ther request
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
 	// These are getting the "name" in the html (not the id or type, even though they are all named the same)
-	fmt.Fprint(w, "Email: ", r.PostForm.Get("email"))
-	fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))
+	// FormValue automatically parses the form, so no need to call the functions to do that
+	// FormValue does not return errors though, so if you need to parse the error then you need to use the other method
+	fmt.Fprint(w, "Email: ", r.FormValue("email"))
+	fmt.Fprint(w, "Password: ", r.FormValue("password"))
 }
