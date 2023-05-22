0f4b4ee93673384786249985eee4059f011e5ebe
Beth MacKay <bmackay@nerdwallet.com>
2023-05-22 11:05:24 -0400

    sec10 postgres config

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -7,10 +7,33 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DbName   string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DbName, cfg.SSLMode)
+}
+
 func main() {
+
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5433",
+		User:     "baloo",
+		Password: "junglebook",
+		DbName:   "lenslocked",
+		SSLMode:  "disable",
+	}
+
 	// pass in driver and conenction string
 	// port matches what's in my docker compose file for the "port on my computer"
-	db, err := sql.Open("pgx", "host=localhost port=5433 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
