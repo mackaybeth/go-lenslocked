810790ec13f974ba8fba526ad02a199eb2992dd3
Beth MacKay <bmackay@nerdwallet.com>
2023-05-29 10:44:20 -0400

    sec12 sign in view, try 1

--- controllers/users.go
+++ controllers/users.go
@@ -9,7 +9,8 @@ import (
 
 type Users struct {
 	Templates struct {
-		New Template
+		New    Template
+		SignIn Template
 	}
 	UserService *models.UserService
 }
@@ -22,6 +23,16 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	data.Email = r.FormValue("email")
 
 	u.Templates.New.Execute(w, data)
+}
+
+func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+
+	u.Templates.SignIn.Execute(w, data)
 }
 
 // Parsing the values from the form using helper methosds in ther request
