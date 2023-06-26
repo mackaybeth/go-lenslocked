8003bbdd088fafba2bce13a002028983859c8dcf
Beth MacKay <bmackay@nerdwallet.com>
2023-06-26 13:02:39 -0400

    sec14: cookie helper funcitons

--- controllers/users.go
+++ controllers/users.go
@@ -47,13 +47,7 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/signin", http.StatusFound)
 		return
 	}
-	cookie := http.Cookie{
-		Name:     "session",
-		Value:    session.Token,
-		Path:     "/",
-		HttpOnly: true,
-	}
-	http.SetCookie(w, &cookie)
+	setCookie(w, CookieSession, session.Token)
 
 	// Redirect to the current user page
 	http.Redirect(w, r, "/users/me", http.StatusFound)
@@ -91,26 +85,21 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cookie := http.Cookie{
-		Name:     "session",
-		Value:    session.Token,
-		Path:     "/",
-		HttpOnly: true, // not accessible to JS
-	}
-	http.SetCookie(w, &cookie)
+	setCookie(w, CookieSession, session.Token)
+
 	http.Redirect(w, r, "/users/me", http.StatusFound)
 
 }
 
 func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
 
-	tokenCookie, err := r.Cookie("session")
+	token, err := readCookie(r, CookieSession)
 	if err != nil {
 		fmt.Println(err)
 		http.Redirect(w, r, "/signin", http.StatusFound)
 		return
 	}
-	user, err := u.SessionService.User(tokenCookie.Value)
+	user, err := u.SessionService.User(token)
 	if err != nil {
 		fmt.Println(err)
 		http.Redirect(w, r, "/signin", http.StatusFound)
