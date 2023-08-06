2fb720992e7a340a3f801ecf3c7ae2142642b4b4
Beth MacKay <bmackay@nerdwallet.com>
2023-08-06 08:07:31 -0400

    sec20-dynamic alerts

--- views/template.go
+++ views/template.go
@@ -38,6 +38,13 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 			"currentUser": func() (template.HTML, error) {
 				return "", fmt.Errorf("currentUser not implemented, add code to Execute to implement")
 			},
+			"errors": func() []string {
+				return []string{
+					"Don't do that!",
+					"The email address you provided is already associated with an account.",
+					"Something went wrong.",
+				}
+			},
 		},
 	)
 
